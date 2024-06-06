ee73d8123a4e02e9ee77bb0c92513e2756728f11
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-06-06 15:52:17 -0300

    feat: setup inicial -Calculadora- -React JS-

---
+++ calculadora/src/components/Calculator.jsx
@@ -0,0 +1,14 @@
+import "./Calculator.css";
+
+const Calculator = () => {
+    return (
+        <div className="calculator">
+            <div className="complete-oparation">3 + 3 = 6</div>
+            <div className="display">6</div>
+            <div className="buttons"></div>
+            <button>=</button>
+        </div>
+    )
+}
+
+export default Calculator;
